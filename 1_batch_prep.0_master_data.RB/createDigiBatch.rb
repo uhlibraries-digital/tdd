bfa7d6e8cdaca9a62ce345f98fd8a0b155d0484e
elihu <7270146+eli-hu@users.noreply.github.com>
2022-09-12 10:39:18 -0500

    Initial commit
    Archive existing createDigiBatch.rb before modification.

---
+++ 1_batch_prep.0_master_data.RB/createDigiBatch.rb
@@ -0,0 +1,58 @@
+require 'fileutils'
+require 'pathname'
+
+def start_wizard(wdir)
+  batch = Time.now.strftime("%Y%m%d")
+  if File.file? "#{wdir}/#{batch}.txt"
+    total_found = 0
+    found = []
+    objects = []
+    File.readlines("#{wdir}/#{batch}.txt").each {|line| objects << line.strip.to_s}
+    total_prepared = objects.size
+    FileUtils.mkdir_p "#{wdir}/#{batch}"
+
+    puts "Create Digi Batch (Pre-1978)"
+    puts "Loading master data from 1_tdd-pre-1978 ..."
+    paths = Dir.glob("#{wdir}/1_tdd-pre-1978/**/**")
+    paths.each do |path|
+      dir = Pathname.new(path)
+      if objects.include? dir.basename.to_s
+        dir_name = dir.basename.to_s
+        FileUtils.mv path, "#{wdir}/#{batch}/#{dir_name}"
+        found << dir_name
+        objects.delete(dir_name)
+      end
+    end
+    if total_prepared == found.size
+      puts "Successfully created batch #{batch} with #{total_prepared} objects: #{found}"
+      puts "Press Enter to exit the application."
+      $stdin.gets.chomp
+    else
+      puts "Created batch #{batch} with #{found.size} objects: #{found}"
+      puts "Could not find #{objects.size} of #{total_prepared} objects: #{objects}"
+      puts "Press Enter to exit the application."
+      $stdin.gets.chomp
+    end
+  else
+    puts "Batch file \'#{batch}.txt\' does not exist. Try again? [Y/N]"
+    response = $stdin.gets.chomp
+    case response
+    when 'Y', 'y', 'Yes', 'yes'
+      start_wizard(wdir)
+    when 'N', 'n', 'No', 'no'
+      exit
+    else
+      puts "Unrecognized input. Exiting application."
+      exit
+    end
+  end
+end
+
+begin
+  start_wizard(Dir.pwd)
+rescue Exception => e
+  File.open("#{Dir.pwd}/exceptions.log", "w") do |f|
+    f.puts e.inspect
+    f.puts e.backtrace
+  end
+end
